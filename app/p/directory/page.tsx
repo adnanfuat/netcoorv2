ec9e6a5bf3e37a106ee15cc83003f9c6ebb9b3f0
Tamer UZUN <58685406+adnanfuat@users.noreply.github.com>
2025-03-26 15:00:23 +0300

    asd

--- app/p/directory/page.tsx
+++ app/p/directory/page.tsx
@@ -4,8 +4,11 @@ import {_userState} from "@/modules/constants/user"
  import { isloggedv4_clerk } from "@/modules/functions/isloggedv4_clerk";
 import permissionsControlV3 from "@/modules/functions/permissionscontrolv3";
 import { Inter } from "next/font/google";
-import UserSelection from "@/components/myaccount/userselection"
-import Memberships_Next15 from "@/modules/memberships_next15";
+import {redirect_postfix } from "@/modules/constants/redirect_postfix";
+
+import Link from "next/link";
+import Image from "next/image";
+import { FaCaretRight } from "react-icons/fa";
 
 const inter = Inter({ subsets: ['latin', "latin-ext"], variable:"--font-inter" }) ; // weight:["100", "200", '300', "400", '500', "600","700", "800", "900"],
 
@@ -22,22 +25,106 @@ export default async function DirectoryPage(context) {
   let technicianAuth     =   userdata?.userscopes.isManager;        
   // console.log("data::::assasa",  noncontrolleds_count)
   let cache =  permissionsControlV3({askList:["cache"], type:"some", permissions:userdata?.permissions});
-  
-  // return (JSON.stringify("Proje seçimi +++ Sektörler Sayfası+ Alt sektörler sayfası aynı yere gitsin.... "));
 
-  return (
-      <div className={s.shell}>                 
-                <Memberships_Next15 
-                        userdata={userdata} 
-                        selecteduser={undefined} 
-                        project={"sakaryarehberim.com"} 
-                        membershipEditUrl="qqqqaaaaaaaaaaaaaaaaaaa" // Zaten bu proje üzerindeyiz ya, o nedenle boş bıraktım.                                                
-                        />
-        </div>
-  );
+  let sectors=[];
+  try {            
+
+        let result = await fetch(process.env.NEXT_PUBLIC_BACKEND_APIURL, {
+        method: "POST",            
+        // next: { revalidate: 100 },
+        headers: { "Content-Type": "application/json", },               
+        body: JSON.stringify({
+          query: SwissArmyKnifeQuery,
+          variables: { data: { type:"companydirectoryv2", project:"sakaryarehberim.com", locale:"tr", defaultLocale:"tr",  imgCloud:(process.env.NEXT_PUBLIC_IMGSOURCE+"/pictures")} },
+        }),
+      })
+        result =  await result.json();        
+        // console.log("result:::::::::", result);
+        sectors=result?.data?.swissarmyknifequery?.tunnel;;       
+
+  } catch (error) {
+    console.log("Firma Rehberi getStaticProps error!!!!: ", error);
+  }
+
+  
+  //  return (JSON.stringify(sectors));
+
+  return ( <List sectors={sectors}/> );
 }
 
 
 
-// membershipEditUrl=router.push(`${membershipEditUrl}${membershipsPath}/c?id=${newcompany?.id}`);
+
+const List = (props) =>{
+
+  let {sectors} = props ?? {}
+
+
+  return <div className={s.shell}   > 
+      
+  {/*  {listsectors?.length} vvvv {sectors?.length}  */}
+  {/*  <HeadComp props={{pagetype:"directory"}}/>    */}
+
+      {sectors?.map((a,i)=>            
+                                    {      
+                                                                                  
+                                     return (
+                                       <div key={i}>             
+                                                    <div className={s.row}> 
+                                                            <Image
+                                                                src={a?.img}
+                                                                alt={a?.title}
+                                                                width={66}
+                                                                height={44}        
+                                                                style={{
+                                                                  objectFit: "cover",
+                                                                  maxWidth: "100%",
+                                                                  height: "auto",
+                                                                  border: "1px solid rgb(184, 184, 184)"
+
+                                                                        }} 
+                                                                />
+
+                                                                <Link
+                                                                  href={`/se/${a?.slug}/${redirect_postfix}`}
+                                                                  prefetch={false}
+                                                                  style={{fontWeight:"bold"}}>
+                                                                  {` ${a?.title}`}
+                                                                </Link>
+                                                      </div>
+
+                                                      <div className={s.s_shell}>
+
+                                                              {a?.subsectors?.map((s,j)=>
+                                                                                    <Link href={`/su/${s?.slug}/${redirect_postfix}`} key={j}>
+                                                                                          <div style={{display:"flex",  alignItems:"center"}}>                                                                                                  
+                                                                                                    <FaCaretRight/>
+                                                                                                    {s?.title}
+                                                                                          </div>
+                                                                                    </Link>
+                                                              )}
+
+                                                      </div>      
+
+                                        </div>
+                                     );
+                                    }
+
+                                  )
+                                  
+                                  }
+               </div>
+}
+
+
+
+
+const SwissArmyKnifeQuery = 
+`  query SwissArmyKnifeQuery ($data:JSON)  {
+    swissarmyknifequery (data:$data) {
+      tunnel
+    }
+  }`
+;
+
 
